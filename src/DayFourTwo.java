85e66fca684ea5318fd382a942d36fadc13ffd74
Mike-Velarde <92482350+Mike-Velarde@users.noreply.github.com>
2024-04-04 11:18:10 -0700

    commit day 1 and 2

--- src/DayFourTwo.java
+++ src/DayFourTwo.java
@@ -1,18 +1,13 @@
 import utils.InputUtils;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class DayFourTwo {
     static ArrayList<String> input;
     static boolean testOrRealData = false;
 
     public static void main(String[] args) {
-        File file = InputUtils.buildFilePath(InputUtils.filePath, InputUtils.fileName, "4", false);
-        input = InputUtils.getInputFromFile(file);
-
         String[] testCards = {
                 "41 48 83 86 17 | 83 86 6 31 17 9 48 53",
                 "13 32 20 16 61 | 61 30 68 82 17 32 24 19",
@@ -22,55 +17,47 @@ public class DayFourTwo {
                 "31 18 13 56 72 | 74 77 10 23 35 67 36 11"
         };
 
-        List<String> cards;
-        cards = testOrRealData ? Arrays.asList(testCards) : input;
+        List<Set<String>> cards = new ArrayList<>();
+        List<Set<String>> winningNumbersList = new ArrayList<>();
 
-        int totalCards = cards.size();
-        List<Integer> cardCopies = new ArrayList<>(cards.size());
-        for (int i = 0; i < cards.size(); i++) {
-            cardCopies.add(0);
+        for (String card : testCards) {
+            String[] parts = card.split("\\|");
+            Set<String> winningNumbers = new HashSet<>(Arrays.asList(parts[0].trim().split(" ")));
+            Set<String> myNumbers = new HashSet<>(Arrays.asList(parts[1].trim().split(" ")));
+            cards.add(myNumbers);
+            winningNumbersList.add(winningNumbers);
         }
 
-        // Process all original and copied scratchcards until no more scratchcards are won
-        boolean wonMoreCards = true;
-        while (wonMoreCards) {
-            wonMoreCards = false;
-            for (int i = 0; i < cards.size(); i++) {
-                int copies = cardCopies.get(i);
-                if (copies > 0) {
-                    // This is a copy of a previously won card, so skip it
-                    continue;
-                }
-                String card = cards.get(i);
-                card = card.replaceAll("\\s(\\d) ", "$1 ");
-                String[] parts = card.split("\\|");
-                List<String> winningNumbers = Arrays.asList(parts[0].trim().split(" "));
-                List<String> myNumbers = Arrays.asList(parts[1].trim().split(" "));
+        int totalCards = cards.size();
+        List<Integer> cardCounts = new ArrayList<>(Collections.nCopies(cards.size(), 1));
 
-                int matches = 0;
+        for (int i = 0; i < cards.size(); i++) {
+            Set<String> myNumbers = cards.get(i);
+            Set<String> winningNumbers = winningNumbersList.get(i);
 
-                for (String number : myNumbers) {
-                    if (winningNumbers.contains(number)) {
-                        matches++;
-                    }
+            int matches = 0;
+            for (String number : myNumbers) {
+                if (winningNumbers.contains(number)) {
+                    matches += cardCounts.get(i);
+                    System.out.println("Card " + (i+1) + " has a match with number: " + number);
                 }
+            }
 
-                // Win copies of subsequent cards equal to the number of matches
-                for (int j = 0; j < matches && i + j + 1 < cards.size(); j++) {
-                    int copyIndex = i + j + 1;
-                    cardCopies.set(copyIndex, cardCopies.get(copyIndex) + 1);
-                    if (cardCopies.get(copyIndex) > 0) {
-                        wonMoreCards = true;
-                    }
-                }
+            // Win copies of subsequent cards equal to the number of matches
+            for (int j = 0; j < matches && i + j + 1 < cards.size(); j++) {
+                int copyIndex = i + j + 1;
+                cardCounts.set(copyIndex, cardCounts.get(copyIndex) + matches);
             }
         }
 
-        // Count the total number of scratchcards
-        for (int copies : cardCopies) {
-            totalCards += copies;
+        // Count the total number of scratchcards and print summary
+        for (int i = 0; i < cardCounts.size(); i++) {
+            int count = cardCounts.get(i);
+            totalCards += count - 1; // Subtract 1 because we've already counted the original card
+            System.out.println("Total count of Card " + (i+1) + ": " + count);
         }
 
         System.out.println("Total cards: " + totalCards);
     }
+
 }
