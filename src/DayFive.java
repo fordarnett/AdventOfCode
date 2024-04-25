2b690b3518d396d3e85063f3149a528d6631e6dd
klaemann <93681820+klaemann@users.noreply.github.com>
2024-04-25 12:27:23 -0500

    Beginning of DayFive
    Start of DayFive work where the cypher has been established

---
+++ src/DayFive.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class DayFive {
+
+    public static Map<Integer, Integer> decodeCypher(int[][] input) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (int[] line : input) {
+            int destinationStart = line[0];
+            int sourceStart = line[1];
+            int rangeLength = line[2];
+            for (int i = 0; i < rangeLength; i++) {
+                result.put(sourceStart + i, destinationStart + i);
+            }
+        }
+        return result;
+    }
+
+    public static int getSoilNumber(Map<Integer, Integer> map, int seedNumber) {
+        return map.getOrDefault(seedNumber, seedNumber);
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {{50, 98, 2}, {52, 50, 48}};
+        Map<Integer, Integer> result = decodeCypher(input);
+        System.out.println(getSoilNumber(result, 79)); // prints 55
+        System.out.println(getSoilNumber(result, 14)); // prints 10
+        System.out.println(getSoilNumber(result, 55)); // prints 50
+        System.out.println(getSoilNumber(result, 13)); // prints 51
+    }
+
+
+}
